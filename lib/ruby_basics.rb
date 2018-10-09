7ffbca99fab1220771ceea706b6f55ba51c61985
Jeffrey Guan <jeffreyg2240@hstat.org>
2018-10-09 18:20:42 +0000

    Hw #5: 01-ruby-basics-lab

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,28 @@
-def division
-    
+def division(x,y)
+    return x/y
 end
 
-def argue
-  
+def argue(x)
+  return x
 end
 
-def greeting
+def greeting(greeting,name)
 
 end
 
-def return_a_value
-    
+def return_a_value()
+  return "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(topping="cheese")
+  return topping;
 end
 
-def whisper
-    
+def whisper(user_input)
+    return user_input.downcase
 end
+
+division(4,1)
+return_a_value
+pizza_party("cheese")
+whisper("Dank")
